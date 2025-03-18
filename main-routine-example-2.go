a1f747541871da27ba49c30323c2e6364479a9de
braniubojni <erik95harutyunyan@gmail.com>
2025-03-18 16:05:20 +0100

    Implement HTTP server with hello handler and concurrent URL pinging example

---
+++ main-routine-example-2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func mainFake2() {
+	path := flag.String("file", "url.txt", "path to URL file")
+	flag.Parse()
+	file, err := os.ReadFile(*path)
+	if err != nil {
+		panic(err.Error())
+	}
+	urlSlice := strings.Split(string(file), "\n")
+	respCh := make(chan int)
+	errCh := make(chan error)
+	for _, url := range urlSlice {
+		go ping(url, respCh, errCh)
+	}
+	for range urlSlice {
+
+		select {
+		case err := <-errCh:
+			fmt.Println(err)
+		case res := <-respCh:
+			fmt.Println(res)
+		}
+
+	}
+}
+
+func ping(url string, respCh chan int, errCh chan error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		errCh <- err
+		return
+	}
+	respCh <- resp.StatusCode
+}
