48d2f685eeb299aa58f5c80cee7af8a03b768bdf
braniubojni <erik95harutyunyan@gmail.com>
2025-03-19 11:40:41 +0100

    Replace hello handler with authentication handler and update configuration to include auth settings

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"go/adv-demo/configs"
-	"go/adv-demo/internal/hello"
+	"go/adv-demo/internal/auth"
 	"net/http"
 )
 
 func main() {
 	conf := configs.LoadConfig()
 	router := http.NewServeMux()
-	hello.NewHelloHandler(router)
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
 
 	server := http.Server{
 		Addr:    ":8081",
