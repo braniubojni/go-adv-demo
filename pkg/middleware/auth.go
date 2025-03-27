5c732be7905794e58a0c544dbd4b4040d2feb016
braniubojni <erik95harutyunyan@gmail.com>
2025-03-27 17:56:14 +0100

    Enhance authentication middleware to handle unauthorized access and improve token validation

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -14,14 +14,27 @@ const (
 	ContextEmailKey key = "ContextEmailKey"
 )
 
+func writeUnauth(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
 func IsLogged(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bearer := "Bearer "
 		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authHeader, bearer) {
+			writeUnauth(w)
+			return
+		}
 		token := strings.TrimPrefix(authHeader, "Bearer ")
-		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !isValid {
+			writeUnauth(w)
+			return
+		}
 		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
 		req := r.WithContext(ctx)
-
 		next.ServeHTTP(w, req)
 	})
 }
